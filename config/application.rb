093db4dcb40a4493378d488405937066c1f9b9e2
Lewisou <lewisou@gmail.com>
2010-10-14 15:42:22 +0800

    add admin stylesheets

--- config/application.rb
+++ config/application.rb
@@ -24,11 +24,11 @@ module Humance
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Beijing'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')] 
+    config.i18n.default_locale = "zh-CN"
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
@@ -37,6 +37,9 @@ module Humance
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
+    
+    config.action_view.javascript_expansions[:jquery] = 
+    %w(jquery-1.4.2.min jquery.ui.core.min jquery.ui.widget.min jquery.ui.tabs.min jquery.tipTip.min jquery.superfish.min jquery.supersubs.min jquery.validate_pack jquery.nyroModal.pack rails application)
   end
 end
